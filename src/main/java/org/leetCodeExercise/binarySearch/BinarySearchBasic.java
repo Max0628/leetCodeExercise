c610c840de660f093010a510f8f438861b44e308
Maxchauo0628 <maxchauo0628@gmail.com>
2025-08-23 13:22:11 +0800

    [feat] add FindFirstGreaterOrEqualIndex methods.

--- src/main/java/org/leetCodeExercise/binarySearch/BinarySearchBasic.java
+++ src/main/java/org/leetCodeExercise/binarySearch/BinarySearchBasic.java
@@ -14,7 +14,7 @@ public class BinarySearchBasic {
    *
    * @return the index of the target int the array, return -1 if not found.
    */
-  public int BinarySearchBasicTwoPointer(int numbers[], int target) {
+  public int BinarySearchTwoPointerBasic(int numbers[], int target) {
     int left = 0;
     int right = numbers.length - 1; // right is inclusive; it is part of the search interval.
     while (left <= right) { // check if left == right as well; exit loop only when left > right.
@@ -29,4 +29,41 @@ public class BinarySearchBasic {
     }
     return -1;
   }
+
+  /**
+   * Instruction:
+   * 1. If you can't find target in array, find the first element which is bigger than target.
+   *
+   * @param numbers
+   * @param target
+   *
+   * @return
+   */
+  public int FindFirstGreaterOrEqualIndex(int numbers[], int target) {
+    int left = 0;
+    int right = numbers.length - 1;
+
+    //Edge case: target is bigger than the biggest element in array.
+    if (target > numbers[right]) {
+      return -1;
+    }
+
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+      if (numbers[middle] > target) {
+        // Current middle is greater than target
+        // It could be the first >= target, but there might be a smaller one on the left
+        right = middle - 1;
+      } else if (numbers[middle] < target) {
+        // Current middle is smaller than target
+        // The first >= target must be to the right of middle
+        left = middle + 1;
+      } else {
+        return middle;
+      }
+    }
+    // At this point, left points to the first element >= target
+    return left;
+  }
 }
+
--- src/test/java/org/leetCodeExercise/binarySearch/BinarySearchBasicTest.java
+++ src/test/java/org/leetCodeExercise/binarySearch/BinarySearchBasicTest.java
@@ -6,7 +6,10 @@ public class BinarySearchBasicTest {
     int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
 
     // condition: target is in the array.
-    int target = 23;
-    System.out.println("Target " + target + " found at index: " + bs.BinarySearchBasicTwoPointer(arr, target));
+    int target1 = 23;
+    System.out.println("Target " + target1 + " found at index: " + bs.BinarySearchTwoPointerBasic(arr, target1));
+
+    int target2 = 38;
+    System.out.println("Target " + target2 + " found at index: " + bs.FindFirstGreaterOrEqualIndex(arr, target2));
   }
 }
